6a0ad6f6148afec2b3c45d2da796ba3be4b537b6
Rashad <rashad.alston@fuel.sh>
2022-05-21 14:48:28 -0700

    Streamline counter-rs program

--- programs/counter-rs/src/main.rs
+++ programs/counter-rs/src/main.rs
@@ -4,14 +4,87 @@ extern crate log;
 extern crate pretty_env_logger;
 
 use actix_web::{middleware, web, web::Bytes, App, Error, HttpRequest, HttpResponse, HttpServer};
-use counter::{get_contract_id, setup_provider_and_wallet, tx_params, Counter};
+use fuel_core::{
+    chain_config::{ChainConfig, CoinConfig, StateConfig},
+    service::{Config, DbType, FuelService},
+};
+use fuel_gql_client::client::FuelClient;
+use fuels::{
+    prelude::{Contract, LocalWallet, Provider, TxParameters, DEFAULT_COIN_AMOUNT},
+    signers::wallet::Wallet,
+    test_helpers::setup_address_and_coins,
+};
+use fuels_abigen_macro::abigen;
 use serde::{Deserialize, Serialize};
+use std::net::{Ipv4Addr, SocketAddr};
 use std::sync::Mutex;
+
+pub fn tx_params() -> TxParameters {
+    let gas_price = 0;
+    let gas_limit = 1_000_000;
+    let byte_price = 0;
+    TxParameters::new(Some(gas_price), Some(gas_limit), Some(byte_price), None)
+}
+
+abigen!(Counter, "./../counter/out/debug/counter-abi.json");
+
+async fn get_contract_id(wallet: &Wallet) -> String {
+    dotenv::dotenv().ok();
+    debug!("Creating new deployment for non-existent contract");
+    let _compiled = Contract::load_sway_contract("./../counter/out/debug/counter.bin").unwrap();
+
+    let bin_path = "./../counter/out/debug/counter.bin".to_string();
+    let contract_id = Contract::deploy(&bin_path, wallet, tx_params())
+        .await
+        .unwrap();
+
+    contract_id.to_string()
+}
+
+async fn setup_provider_and_wallet() -> (Provider, LocalWallet) {
+    let (secret, coins) = setup_address_and_coins(1, DEFAULT_COIN_AMOUNT);
+
+    let coin_configs = coins
+        .into_iter()
+        .map(|(utxo_id, coin)| CoinConfig {
+            tx_id: Some(*utxo_id.tx_id()),
+            output_index: Some(utxo_id.output_index() as u64),
+            block_created: Some(coin.block_created),
+            maturity: Some(coin.maturity),
+            owner: coin.owner,
+            amount: coin.amount,
+            asset_id: coin.asset_id,
+        })
+        .collect();
+
+    let config = Config {
+        chain_conf: ChainConfig {
+            initial_state: Some(StateConfig {
+                coins: Some(coin_configs),
+                ..StateConfig::default()
+            }),
+            ..ChainConfig::local_testnet()
+        },
+        database_type: DbType::InMemory,
+        utxo_validation: false,
+        addr: SocketAddr::new(Ipv4Addr::new(127, 0, 0, 1).into(), 4000),
+        ..Config::local_node()
+    };
+
+    let srv = FuelService::new_node(config).await.unwrap();
+    let client = FuelClient::from(srv.bound_address);
+    info!("Fuel client started at {:?}", client);
+
+    let provider = Provider::new(client);
+    let wallet = LocalWallet::new_from_private_key(secret, provider.clone());
+
+    (provider, wallet)
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 struct IncrementCountResponse {
     success: bool,
-    count: u32,
+    count: u64,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -22,19 +95,19 @@ struct IncrementCountRequest {
 #[derive(Debug, Serialize, Deserialize)]
 struct GetCountResponse {
     success: bool,
-    count: u32,
+    count: u64,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct InitCountResponse {
     success: bool,
-    count: u32,
+    count: u64,
 }
 
 async fn initialize_count(req: HttpRequest) -> Result<HttpResponse, Error> {
     let resp = InitCountResponse {
         success: true,
-        count: 1u32,
+        count: 1u64,
     };
     let state = req.app_data::<web::Data<Mutex<Counter>>>().unwrap();
     let contract = match state.lock() {
@@ -56,7 +129,7 @@ async fn initialize_count(req: HttpRequest) -> Result<HttpResponse, Error> {
 
     debug!("{:?}", result);
 
-    let count: u32 = result.receipts[1].val().unwrap();
+    let count: u64 = result.receipts[1].val().unwrap();
     Ok(HttpResponse::Ok().content_type("application/json").body(
         serde_json::to_string(&InitCountResponse {
             success: true,
@@ -77,7 +150,7 @@ async fn increment_count(req: HttpRequest, body: Bytes) -> Result<HttpResponse, 
             return Ok(HttpResponse::Ok().content_type("application/json").body(
                 serde_json::to_string(&IncrementCountResponse {
                     success: false,
-                    count: 0u32,
+                    count: 0u64,
                 })
                 .unwrap(),
             ));
@@ -92,7 +165,7 @@ async fn increment_count(req: HttpRequest, body: Bytes) -> Result<HttpResponse, 
         .unwrap();
 
     debug!("{:?}", result);
-    let count: u32 = result.receipts[1].val().unwrap();
+    let count: u64 = result.receipts[1].val().unwrap();
 
     Ok(HttpResponse::Ok().content_type("application/json").body(
         serde_json::to_string(&IncrementCountResponse {
@@ -115,7 +188,7 @@ async fn get_count(req: HttpRequest) -> Result<HttpResponse, Error> {
         .unwrap();
 
     debug!("{:?}", result);
-    let count: u32 = result.receipts[1].val().unwrap();
+    let count: u64 = result.receipts[1].val().unwrap();
 
     Ok(HttpResponse::Ok().content_type("application/json").body(
         serde_json::to_string(&GetCountResponse {
